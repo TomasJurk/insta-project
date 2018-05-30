0b121ed68a6191ca5208d6ec8ae4efde48d47fd6
TomasJurk <tomasstj@gmail.com>
2018-05-30 14:32:41 +0300

    listing of saved influencers done, from database

--- src/app/admin/influencers/influencers.component.ts
+++ src/app/admin/influencers/influencers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Parse from 'parse';
 
 @Component({
   selector: 'app-influencers',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfluencersComponent implements OnInit {
 
+  influencers = [];
   constructor() { }
 
   ngOnInit() {
+  }
+
+    // http://docs.parseplatform.org/js/guide/#queries
+
+  getInfluencers() {
+    this.influencers = [];
+    const Influencers4201 = Parse.Object.extend('Influencers4201');
+    const query = new Parse.Query(Influencers4201);
+    query.find({
+      success: (results) => {
+        console.log('Successfully retrieved ' + results.length);
+        for (let i = 0; i < results.length; i++) {
+          const object = results[i];
+          this.influencers.push(object.attributes);
+          console.log(this.influencers);
+        }
+      },
+      error: (error) => {
+        alert('Error: ' + error.code + ' ' + error.message);
+      }
+    });
   }
 
 }
